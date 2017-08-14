5dfac7a8e884f05653f5f14fadd3ab68016e3491
victor1822 <30930332+victor1822@users.noreply.github.com>
2017-08-14 19:02:05 -0300

    Update Ordenacao.cpp

--- Ordenacao/Ordenacao.cpp
+++ Ordenacao/Ordenacao.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <fstream>
 #include <cstdio>
-#include "print_vetor.hpp"
-#include "SelectionSort.hpp"//ss(x) //type(x) = std::vector
+#include "print_vetor.hpp"//printv(x) //type(x) = std::vector (recebe vetor de inteiros e imprime seu conteúdo sequencialmente)
+#include "SelectionSort.hpp"//ss(x) //type(x) = std::vector(recebe um vetor e ordena seus valores, assim como qs(),bs() e is())
 #include "QuickSort.hpp"//qs(x,c,f) type(x) = std::vector, type(c) = int(começo) type(f) = int(fim)
 #include "BubbleSort.hpp"//bs(vector<int>x) type(x) = std::vector
 #include "IsertionSort.hpp"//is(x) type(x) = std::vector
@@ -30,7 +30,7 @@ int main()
     file2.open("output.txt");
 //implementar heuristica aq <3
 cout<<endl<<"O tamanho do vetor eh:"<<e.size()<<endl;
-printv(e);//printv() recebe um vetor de entrada e imprime seus valores sequencialmente
+printv(e);
 vector<int>c=e;
 vector<int>b=e;
 vector<int>i=e;
