11a75a7d39beb8ccb30c714bd83fe8c007956675
victor1822 <30930332+victor1822@users.noreply.github.com>
2017-09-14 12:51:07 -0300

    #5 commit

--- Ordenacao/CountSort.hpp
+++ Ordenacao/CountSort.hpp
@@ -39,41 +39,29 @@ free(tmp);
 free(resposta);
 }
 
-int cs(vector<int> &ent, int exp){ //counting sort utilizado no radix
-//int vector<int> r;
-int *resposta = (int*)malloc((ent.size()-1)*sizeof(int));
-int menor,maior;
-menor=maior=ent[0];
-for(int i=0;i<ent.size()-1;i++){
-if(ent[i]<menor)menor=ent[i];
-if(ent[i]>maior)maior=ent[i];
-}
-int v=maior-menor+1;
-int *tmp  = (int*)malloc(v*sizeof(int));
-
-//int count[10]={0}
-
-for(int i=0;i<v;i++){
-tmp[i]=0;
-}//zerando o vetor temporario
-
-for(int i=0;i<ent.size()-1;i++){
-tmp[((ent[i]-menor)/exp)%(ent.size()-1)]++;
-}//contando
-
-for(int i=1;i<v;i++){
-tmp[i]=tmp[i]+tmp[i-1];
-}//somando
-
-for(int i=0;i<ent.size()-1;i++){
-resposta[tmp[((ent[i]-menor)/exp)%(ent.size()-1)]-- -1]=ent[i];
-}
-
-for(int i=0;i<ent.size()-1;i++){
-ent[i]=resposta[i];
-}
-free(tmp);
-free(resposta);
+int cs(int arr[], int n, int exp)
+{
+    int output[n]; // output array
+    int i, count[10] = {0};
+ 
+    //contador de ocorrencias
+    for (i = 0; i < n; i++)
+        count[ (arr[i]/exp)%10 ]++;
+ 
+//calcula posicao correta no array de saida
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+ 
+    // constroi o array de saida
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+        count[ (arr[i]/exp)%10 ]--;
+    }
+ 
+//passa os valores corretos para o array
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
 }
 
 
--- Ordenacao/RadixSort.hpp
+++ Ordenacao/RadixSort.hpp
@@ -15,13 +15,37 @@ int getMax(vector<int> ent)
     return mx;
 }
 
+int getMin(vector<int> ent){
+   int mx = ent[0];
+    for (int i = 1; i < ent.size()-1; i++)
+        if (ent[i] < mx)
+            mx = ent[i];
+    return mx;
+}
+
+void shift(vector<int> &ent,int m)
+{
+    for (int i = 0; i < ent.size()-1; i++)
+ 	ent[i]=ent[i]-m;
+}
+
 void rs(vector<int> &ent)
 {
     // Find the maximum number to know number of digits
-    int m = getMax(ent);
- 
-    for (int exp = 1; m/exp > 0; exp *= 10)
-        cs(ent, exp);//chama o counting sort
+
+int min = getMin(ent);//pega o menor
+//cout<<"min"<<min;
+if(min<0)shift(ent,min);//se for menor que zero, da um shift
+int *tmp=(int*)malloc((ent.size()-1)*sizeof(int));//aloca um array de inteiros para entrar na funcao
+for(int i=0;i<ent.size()-1;i++)//passa os valores do vetor para o array
+tmp[i]=ent[i];
+    int max = getMax(ent);//pega o valor maximo para determinar o numero de digitos
+    for (int exp = 1; max/exp > 0; exp *= 10)
+        cs(tmp,ent.size()-1, exp);//chama o counting sort
+for(int i=0;i<ent.size()-1;i++)
+ent[i]=tmp[i];
+if(min<0)shift(ent,-min);
+free(tmp);
 }
 
 
